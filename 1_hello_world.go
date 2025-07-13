0c865ae4659ab6cc01242170bcab6b264268222c
Ankan mandal <mail2bongguy@gmail.com>
2025-07-13 23:41:03 +0530

    First commit

---
+++ 1_hello_world.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// String
+	fmt.Println("Hello World")
+
+	// Number
+	fmt.Println(1 + 1)
+
+	//Bool
+	fmt.Println(true)
+
+	//Floats
+	fmt.Println(10.6)
+	fmt.Println(7.0 / 3.0)
+}
---
+++ 2_variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Golang"
+	var age = 123
+	fmt.Println(name, age)
+
+	// shorthand syntax
+	name1 := "ankan"
+	fmt.Println(name1)
+
+	var fullName string
+	fullName = "Ankan Mandal"
+	fmt.Println(fullName)
+}
